c7c9edfc61c64536a9cf1a8315167b26efe7ceed
dg <dg>
2013-01-27 23:11:18 +0000

    Added 'noise' sample program.

--- Makefile
+++ Makefile
@@ -2,12 +2,15 @@ CXX = clang++
 LLVM = -I$(shell llvm-config-3.2 --includedir) -lLLVM-3.2
 #LLVM = $(shell /tmp/llvm/bin/llvm-config --cflags --ldflags --libs)
 
-all: ctest fractal
+all: ctest fractal noise
 
 ctest: ctest.cc Makefile $(wildcard calculon*.h)
 	$(CXX) -g -o $@ $< $(LLVM)
 	
 fractal: fractal.cc Makefile $(wildcard calculon*.h)
 	$(CXX) -g -o $@ $< $(LLVM) -lboost_program_options
+	
+noise: noise.cc Makefile $(wildcard calculon*.h)
+	$(CXX) -g -o $@ $< $(LLVM) -lnoise -lboost_program_options
 	
 	
---
+++ noise.cc
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <boost/program_options.hpp>
+#include "libnoise/noise.h"
+#include "calculon.h"
+
+using std::string;
+namespace po = boost::program_options;
+
+typedef Calculon::Instance<Calculon::RealIsFloat> Compiler;
+typedef Compiler::Real Real;
+typedef Compiler::Vector Vector;
+
+Compiler::StandardSymbolTable symbols;
+
+extern "C"
+double perlin(Vector* v)
+{
+	static noise::module::Perlin generator;
+	return generator.GetValue(v->x, v->y, v->z);
+}
+
+int main(int argc, const char* argv[])
+{
+	po::options_description options("Allowed options");
+	options.add_options()
+	    ("help,h",
+	    		"produce help message")
+	    ("width,x",  po::value<int>()->default_value(1024),
+	    		"width of output image")
+	    ("height,y", po::value<int>()->default_value(1024),
+	    		"height of output image")
+	    ("file,f",   po::value<string>()->default_value("noise.cal"),
+	    		"input Calculon script name")
+	    ("output,o", po::value<string>()->default_value("noise.ppm"),
+	    		"output filename")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, options), vm);
+	po::notify(vm);
+
+	if (vm.count("help"))
+	{
+		std::cout << options << "\n";
+		exit(1);
+	}
+
+	int width = vm["width"].as<int>();
+	int height = vm["height"].as<int>();
+	string scriptfilename = vm["file"].as<string>();
+	string outputfilename = vm["output"].as<string>();
+
+	/* Register the noise function. */
+
+	symbols.add("perlin", "D=V", perlin);
+
+	/* Load the Calculon function to generate the pixels. */
+
+	typedef void FractalFunction(Vector* result, Real r, Real i);
+	std::ifstream code(scriptfilename.c_str());
+	Compiler::Program<FractalFunction> func(symbols, code, "(x,y):vector");
+	func.dump();
+
+	/* Open the output file. */
+
+	std::ofstream outputfile(outputfilename.c_str());
+	outputfile << "P3\n" << width << "\n" << height << "\n" << "65535\n";
+
+	for (Real y = 0; y < height; y++)
+	{
+		for (Real x = 0; x < width; x++)
+		{
+			Real xx = -1 + x * (2/(Real)width);
+			Real yy = -1 + y * (2/(Real)height);
+
+			Vector result;
+			func(&result, xx, yy);
+
+			outputfile << (int)(result.x * 65535.0) << " "
+					   << (int)(result.y * 65535.0) << " "
+					   << (int)(result.z * 65535.0) << "\n";
+		}
+	}
+
+	return 0;
+}
